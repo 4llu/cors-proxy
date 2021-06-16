b2dc47f1f4a186e5837eeb9b2fc0b481d0b0ca55
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2021-06-16 15:01:20 +0300

    Generalize to non-Reaktor urls

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
-const express = require('express');
-const request = require('request');
+const express = require("express");
+const request = require("request");
 
 const app = express();
-const proxyTo = "https://bad-api-assignment.reaktor.com"
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.get('**', (req, res) => {
-  let realPath = proxyTo + req.params[0]
-  console.log(realPath);
-  request(
-    { url: realPath },
-    (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        return res.json(JSON.parse(body));
-      }
-      else if (error) {
-        return res.status(500).json({ type: 'error', message: error.message });
-      }
-      else {
-        return res.status(response.statusCode).json({ type: 'error', message: response.statusCode });
-      }
-
+app.get("**", (req, res) => {
+  let path = req.params[0].slice(1, req.params[0].length);
+  const queries = Object.keys(req.query)
+    .map((q) => `${q}=${req.query[q]}`)
+    .join("&");
+  const fullUrl = `${path}?${queries}`;
+  console.log(fullUrl);
+  request({ url: fullUrl }, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      return res.json(JSON.parse(body));
+    } else if (error) {
+      return res.status(500).json({ type: "error", message: error.message });
+    } else {
+      return res
+        .status(response.statusCode)
+        .json({ type: "error", message: response.statusCode });
     }
-  )
+  });
 });
 
 const PORT = process.env.PORT || 3001;
