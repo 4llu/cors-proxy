a89b3433d6a0d419b72b7d51e2991e81b7b53c7d
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2021-06-21 15:44:56 +0300

    Fix query parameter handling

--- server.js
+++ server.js
@@ -4,28 +4,26 @@ const request = require("request");
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
 });
 
 app.get("**", (req, res) => {
-  let path = req.params[0].slice(1, req.params[0].length);
-  const queries = Object.keys(req.query)
-    .map((q) => `${q}=${req.query[q]}`)
-    .join("&");
-  const fullUrl = `${path}?${queries}`;
-  console.log(fullUrl);
-  request({ url: fullUrl }, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      return res.json(JSON.parse(body));
-    } else if (error) {
-      return res.status(500).json({ type: "error", message: error.message });
-    } else {
-      return res
-        .status(response.statusCode)
-        .json({ type: "error", message: response.statusCode });
-    }
-  });
+    const url = req.url.slice(1, req.url.length);
+    console.log(url);
+    request({ url }, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            return res.json(JSON.parse(body));
+        } else if (error) {
+            return res
+                .status(500)
+                .json({ type: "error", message: error.message });
+        } else {
+            return res
+                .status(response.statusCode)
+                .json({ type: "error", message: response.statusCode });
+        }
+    });
 });
 
 const PORT = process.env.PORT || 3001;
